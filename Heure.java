16dd13d3d87d1ab9063effc7c4feec6251a4cc94
eleve <rehane.karadjia.sio@gmail.com>
2017-09-06 15:33:53 +0200

    ajout des getters

--- Heure.java
+++ Heure.java
@@ -6,5 +6,15 @@ public class Heure {
 		this.heures=heures;
 		this.minutes=minutes;
 	}	
+	
+	public int getHeures(){
 
+ 		return this.heures;
+	}
+		
+	public int getMinutes(){
+
+		return this.minutes;
+
+	}
 };
